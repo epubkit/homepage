c18ed8a3a93e03a1543b0c061c3aec1fbbab772c
Randy <randypriv@gmail.com>
2024-04-24 11:42:47 +0800

    add seo article

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://epubkit.app",
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
 	integrations: [tailwind()],
 });
