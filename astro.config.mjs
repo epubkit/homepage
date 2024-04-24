28cd31939c79250bca24f293a34946edb9c957b1
Randy <randypriv@gmail.com>
2024-04-24 11:48:31 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://epubkit.app",
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en',
-	},
-	integrations: [tailwind()],
+  site: "https://epubkit.app",
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en'
+  },
+  integrations: [tailwind(), sitemap()]
 });
