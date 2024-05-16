4a9d05f0b86b03f6fbbad706984ccb99fd72bbcc
Randy <randypriv@gmail.com>
2024-05-16 19:06:57 +0800

    lang

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
     locales: ['en'],
     defaultLocale: 'en'
   },
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), react()]
 });
