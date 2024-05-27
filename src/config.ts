7d7a603507f2eedebf9368cb4373c91db89c27da
Randy <randypriv@gmail.com>
2024-05-27 22:45:54 +0800

    migrate to stripe

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 export const config = {
-  buyLink: "https://randynamic-studio.lemonsqueezy.com/buy/4481bfe1-eded-4bcd-a182-418e733871b9",
   extension: {
     chrome: {
       link: "https://chromewebstore.google.com/detail/epubkit-connect/kolfkabggcnighackpbmmdolojkffapk",
